616724210556c6192e90c196810faa8da775e3a8
guptajimahak <55080310+guptajimahak@users.noreply.github.com>
2021-08-10 23:26:29 +0530

    fff

---
+++ assets/js/Product-Slider-1.js
@@ -0,0 +1,18 @@
+	$(document).ready(function() {
+
+
+			$('#slider').layerSlider({
+				sliderVersion: '6.6.3',
+				autoStart: false,
+				pauseOnHover: 'disabled',
+				keybNav: false,
+				skin: 'v6',
+				globalBGSize: 'cover',
+				navPrevNext: false,
+				hoverPrevNext: false,
+				navStartStop: false,
+				navButtons: false,
+				showCircleTimer: false,
+				skinsPath: '../../layerslider/skins/'
+			});
+		});
