11ba50076b875c8bd3df5b0201b9a601ae855006
guptajimahak <55080310+guptajimahak@users.noreply.github.com>
2021-08-28 13:47:42 +0530

    hioo

--- assets/js/Card-slider-sk.js
+++
@@ -1,6 +0,0 @@
-(function($){
-	   "use strict";
-	   $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-	   $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-   })	
-   (jQuery);
--- assets/js/Media-Slider-Bootstrap-3.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('#Carousel').carousel({
-        interval: 5000
-    })
-});
