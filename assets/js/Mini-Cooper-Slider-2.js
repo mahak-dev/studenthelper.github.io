e55a2ff2ec94c69cdaecacbd306ede98c6bea951
guptajimahak <55080310+guptajimahak@users.noreply.github.com>
2021-08-28 13:48:08 +0530

    ddrr

---
+++ assets/js/Mini-Cooper-Slider-2.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+			
+			$('#slider').layerSlider({
+				sliderVersion: '6.0.0',
+				type: 'fullsize',
+				responsiveUnder: 0,
+				maxRatio: 1,
+				hideUnder: 0,
+				hideOver: 100000,
+				skin: 'outline',
+				sliderFadeInDuration: 800,
+				tnContainerWidth: '100%',
+				tnWidth: 170,
+				tnHeight: 100,
+				skinsPath: '../../layerslider/skins/',
+				height: 600
+			});
+		});
